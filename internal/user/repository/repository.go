a199a5c5aae53c502e030abc55d6b4c5c66d7ba6
Yan Hairul Firdan <yanhairulfirdan@gmail>
2025-05-28 05:03:07 +0800

    feat: enhance repository with error handling for user lookup

--- internal/user/repository/repository.go
+++ internal/user/repository/repository.go
@@ -6,8 +6,17 @@ import (
 	"go-campaign.com/internal/user"
 )
 
+type Filter struct {
+	Column   string
+	Value    any
+	Operator string
+}
+
+type Filters []Filter
+
 type Repository interface {
 	Create(user.User) (user.User, error)
+	FindBy(column string, value any) (user.User, error)
 }
 
 func NewRepository(connection *sql.DB) Repository {
