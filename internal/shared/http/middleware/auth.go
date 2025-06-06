65129808580bc69b48af7dcad419a206fefd3cfe
Yan Hairul Firdan <yanhairulfirdan@gmail>
2025-06-06 20:45:33 +0800

    feat: add token extraction and validation middleware for user authentication

--- internal/shared/http/middleware/auth.go
+++ internal/shared/http/middleware/auth.go
@@ -5,6 +5,8 @@ import (
 
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"go-campaign.com/pkg/auth"
 )
 
 func Protected() fiber.Handler {
@@ -22,4 +24,25 @@ func Protected() fiber.Handler {
 			return c.Next()
 		},
 	})
+}
+
+func ExtractToken(c *fiber.Ctx) error {
+	jwtToken := c.Locals("user").(*jwt.Token)
+
+	if jwtToken == nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error":   "Unauthorized",
+			"message": "No token provided",
+		})
+	}
+
+	userID, ok := auth.ValidateToken(jwtToken.Raw)
+	if ok != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error":   "Unauthorized",
+			"message": "Invalid token",
+		})
+	}
+	c.Locals("userID", userID)
+	return c.Next()
 }
