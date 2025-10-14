cc50ad62472e658de673328d709f997890eb4606
Yan Hairul Firdan <yanhairulfirdan@gmail>
2025-10-14 09:20:36 +0800

    remove unused codes

--- internal/shared/pagination/pagination.go
+++
@@ -1,6 +0,0 @@
-package pagination
-
-type Builder struct {
-	DataRetriever  func(page, perPage int) ([]any, error)
-	CountRetriever func() (int, error)
-}
--- pkg/validation/validation.go
+++ pkg/validation/validation.go
@@ -1,9 +1,27 @@
 package validation
 
+import "errors"
+
+type Rule = string
+type Validator = func(value interface{}) error
+
 var (
-	databaseRepository DatabaseValidationRepository
+	databaseRepository   DatabaseValidationRepository
+	ErrValidatorNotFound = errors.New("validator not found")
+	validators           = make(map[Rule]Validator)
 )
 
 func Init(db DatabaseValidationRepository) {
 	databaseRepository = db
+}
+
+func GetValidator(name string) (Validator, error) {
+	if v, ok := validators[name]; ok {
+		return v, nil
+	}
+	return nil, ErrValidatorNotFound
+}
+
+func RegisterValidator(name string, validator Validator) {
+	validators[name] = validator
 }
