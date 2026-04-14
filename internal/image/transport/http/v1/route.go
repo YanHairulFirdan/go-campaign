a4874ce8d7e01a03c04dab5ea9fbdb3db0f37838
Yan Hairul Firdan <yanhairulfirdan@gmail>
2026-04-14 13:26:14 +0800

    feat: add middleware protection to image upload route

--- internal/image/transport/http/v1/route.go
+++ internal/image/transport/http/v1/route.go
@@ -2,10 +2,11 @@ package v1
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"go-campaign.com/internal/shared/http/middleware"
 )
 
 func RegisterRouteV1(apiV1 fiber.Router) {
-	uploadGroup := apiV1.Group("/images")
+	uploadGroup := apiV1.Group("/images", middleware.Protected(), middleware.ExtractToken)
 	uploadGroup.Post("/upload", Upload)
 	uploadGroup.Delete("/delete", Delete)
 }
