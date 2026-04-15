5f43ea721e1fc7c69df6420331a5105bc980d31f
Yan Hairul Firdan <yanhairulfirdan@gmail>
2026-04-15 11:17:27 +0800

    feat: add pagination query normalizer middleware for improved query handling

---
+++ internal/shared/http/middleware/pagination_normalizer.go
@@ -0,0 +1,28 @@
+package middleware
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type QueryNormalization map[string]int
+
+func PaginationQueryNormalizer(q QueryNormalization) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		args := ctx.Request().URI().QueryArgs()
+
+		for k, minValue := range q {
+			pageValue := strings.TrimSpace(string(args.Peek("page")))
+
+			if pageValue != "" {
+				if num, err := strconv.Atoi(pageValue); err != nil || num < minValue {
+					args.Set(k, strconv.Itoa(minValue))
+				}
+			}
+		}
+
+		return ctx.Next()
+	}
+}
